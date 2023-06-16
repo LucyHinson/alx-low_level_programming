dd3fd60f67ee20eb246fb058762cf3d4e4f02b86
LucyHinson <nanamahinson02@gmail.com>
2023-06-16 07:03:06 -0700

    100-intel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,1 @@
-#include <stdio.h>
-/**
- * main - A program that generates the assembly code
- * Return: 0 (`Success)
- */
-int main(void)
-{
-	printf("size of char: %d byte(s)", sizeof(char));
-	printf("size of int: %d byte(s)", sizeof(int));
-	printf("size of a long int: %d byte(s)", sizeof(long int));
-	printf("size of a long long int: %d  byte(s)", sizeof(long long int));
-	printf("size of a float: %d byte(s)", sizeof(float));
-	return (0);
+
